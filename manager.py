1390d1602e38c485b7a0d93625ac09e56fd36862
Jay-wm <55948899+Jay-wm@users.noreply.github.com>
2020-05-11 08:25:37 +0800

    将基金经理信息单独成类

--- manager.py
+++ manager.py
@@ -5,17 +5,17 @@ class Manager():
 
     def get_manager_name(self):
         '''获取当前基金经理名称'''
-        manager_name = self.selector2.xpath('//tbody/tr[1]/td[3]/a/text()')[0]
+        manager_name = self.selector.xpath('//tbody/tr[1]/td[3]/a/text()')[0]
         return manager_name
 
 
     def get_manage_time(self):
         '''获得基金经理任职时长'''
-        manage_time = self.selector2.xpath('//tbody/tr[1]/td[4]/text()')[0]
+        manage_time = self.selector.xpath('//tbody/tr[1]/td[4]/text()')[0]
         return manage_time
 
 
     def get_manage_reward(self):
         '''获得基金经理任职回报'''
-        manage_reward = self.selector2.xpath('//tbody/tr[1]/td[5]/text()')[0]
+        manage_reward = self.selector.xpath('//tbody/tr[1]/td[5]/text()')[0]
         return manage_reward
